6c545947887d290d8a575f8c737a5dd7b64afc64
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-10-30 23:42:18 +0100

    Chore: some updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import RouterConfig from "@/routes/routeConfig";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <>
-      <RouterConfig />
+      <Provider store={store}>
+        <BrowserRouter>
+          <RouterConfig />
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- src/components/primitives/auth/auth-left/AuthLeft.tsx
+++ src/components/primitives/auth/auth-left/AuthLeft.tsx
@@ -2,15 +2,12 @@ import "./AuthLeft.scss";
 
 const AuthLeft = () => {
   return (
-    <div className="auth_left_side center">
+    <div className="auth_left_side center col">
+      <div className="auth_left_side_text_wrap">Welcome to Conditron</div>
       <div className="auth_left_side_text_wrap">
-        <span className="auth_left_side_text_light">We Move Rides</span>
-        <br />
-        Your location, we move...
+        Your one stop platform for your asset risk assesment
       </div>
-      {/* <div className="auth_left_side_float"></div> */}
       <div className="auth_left_side_float_one"></div>
-      {/* <div className="auth_left_side_float_two"></div> */}
     </div>
   );
 };
